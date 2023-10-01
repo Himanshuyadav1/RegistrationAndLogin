16c23e0d0a1f3d912f87a932d2443643fbc9b0d0
Himanshu Yadav <himanshu.tj@gmail.com>
2023-10-01 22:21:52 +0530

    Updated the views

--- routes/router.js
+++ routes/router.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render("homepage");
+    res.render("homepage", { title: "Home Page" });
 });
 
 router.get('/login', (req, res) => {
-    res.render("login");
+    res.render("login", { title: "Login Page" });
 });
 
 router.get('/registration', (req, res) => {
-    res.render("registration");
+    res.render("registration", { title: "Registration Page" });
 });
 
 module.exports = router;
